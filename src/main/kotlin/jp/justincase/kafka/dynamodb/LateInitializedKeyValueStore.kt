9c79484a33b77440c6309ab11f1272a79c15cf07
Nicholas Yip <nicholas.yip@justincase-tech.com>
2019-10-13 22:35:26 +0900

    Implement `LateInitializedKeyValueStore`

---
+++ src/main/kotlin/jp/justincase/kafka/dynamodb/LateInitializedKeyValueStore.kt
@@ -0,0 +1,27 @@
+package jp.justincase.kafka.dynamodb
+
+import org.apache.kafka.streams.KeyValue
+import org.apache.kafka.streams.processor.ProcessorContext
+import org.apache.kafka.streams.processor.StateStore
+import org.apache.kafka.streams.state.KeyValueStore
+
+class LateInitializedKeyValueStore<K, V>(private val factory: () -> KeyValueStore<K, V>) : AbstractKeyValueStore<K, V> {
+  private lateinit var delegate: KeyValueStore<K, V>
+
+  override fun init(context: ProcessorContext, root: StateStore) {
+    delegate = factory()
+    super.init(context, root)
+  }
+
+  override fun isOpen() = delegate.isOpen
+  override fun close() = delegate.close()
+  override fun name(): String = delegate.name()
+  override fun persistent() = delegate.persistent()
+  override fun approximateNumEntries() = delegate.approximateNumEntries()
+
+  override fun put(key: K, value: V) = delegate.put(key, value)
+  override fun putAll(entries: List<KeyValue<K, V>>) = delegate.putAll(entries)
+  override fun putIfAbsent(key: K, value: V): V? = delegate.putIfAbsent(key, value)
+  override fun get(key: K): V? = delegate.get(key)
+  override fun delete(key: K): V? = delegate.delete(key)
+}
